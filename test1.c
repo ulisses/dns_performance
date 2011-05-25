cce7d14fd1563104c926339a2a97545c785c5bf5
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-25 01:47:46 +0100

    closer and closer...

--- test1.c
+++ test1.c
@@ -14,10 +14,9 @@ static int usage(FILE *fp, char *prog) {
 int main(int argc, char *argv[]) {
 	ldns_resolver *res;
 	ldns_rdf *name;
-	ldns_rdf *version, *id;
+	ldns_rdf *version;
 	ldns_pkt *p;
 	ldns_rr_list *addr;
-	ldns_rr_list *info;
 	ldns_status s;
 	ldns_rdf *pop;
 	size_t i;
@@ -28,7 +27,8 @@ int main(int argc, char *argv[]) {
 	} else {
 		/* Create a rdf from the command line arg */
 		name = ldns_dname_new_frm_str(argv[1]);
-		ldns_dname2canonical(name);
+		//ldns_dname2canonical(name);
+		//ldns_rdf_set_type(name, 255);
 		if (!name) {
 			usage(stdout, argv[0]);
 			exit(EXIT_FAILURE);
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Create rdf for what we are going to ask */
-	version = ldns_dname_new_frm_str("query example");
+	version = ldns_dname_new_frm_str("google.com");
 
 	/* Create a new resolver from /etc/resolv.conf */
 	s = ldns_resolver_new_frm_file(&res, NULL);
@@ -45,15 +45,23 @@ int main(int argc, char *argv[]) {
 		ldns_rdf_deep_free(name);
 		exit(EXIT_FAILURE);
 	}
-	/* Force recursion, we want the first DNS server to do the all the job*/
+
+	//res = ldns_resolver_new();
+	//ldns_resolver_push_searchlist(res,name);
+	p = ldns_resolver_query(res, name, LDNS_RR_TYPE_NS,LDNS_RR_CLASS_IN, LDNS_RD);
+
+	/* Force recursion, we want the first DNS server to do the all the job (if possible)*/
 	if(!ldns_resolver_recursive(res)) {
-		ldns_resolver_set_recursive(res,false);
+		ldns_resolver_set_recursive(res,true);
 	}
 	ldns_resolver_set_retry(res, 1); /* don't want to wait too long */
+	ldns_pkt_print(stdout,p);
 
-	/* use the resolver to send it a query for the a/aaaa of name */
-	addr = ldns_get_rr_list_addr_by_name(res, name, LDNS_RR_CLASS_IN, LDNS_RD); /* LDNS_RD: Recursion Desired - query flag */
-	//addr = ldns_get_rr_list_name_by_addr(res, name, LDNS_RR_CLASS_IN, LDNS_RD); /* LDNS_RD: Recursion Desired - query flag */
+	addr = ldns_pkt_answer(p);
+
+	// use the resolver to send it a query for the a/aaaa of name 
+	//addr = ldns_get_rr_list_addr_by_name(res, name, LDNS_RR_CLASS_IN, LDNS_RD); // LDNS_RD: Recursion Desired - query flag 
+	//addr = ldns_get_rr_list_name_by_addr(res, name, LDNS_RR_CLASS_IN, LDNS_RD); // LDNS_RD: Recursion Desired - query flag 
 	if (!addr) {
 		fprintf(stderr, " *** could not get an address for %s\n", argv[1]);
 		ldns_rdf_deep_free(name);
@@ -61,21 +69,21 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	/* remove current list of nameservers from resolver */
-	while((pop = ldns_resolver_pop_nameserver(res))) {ldns_rdf_deep_free(pop);}
+	// remove current list of nameservers from resolver 
+	//while((pop = ldns_resolver_pop_nameserver(res))) {ldns_rdf_deep_free(pop);}
 
-	/* can be multihomed */
+	// can be multihomed 
+	
+	printf("we have:%d nameservers\n",(int)ldns_rr_list_rr_count(addr));
+	
 	for(i = 0; i < ldns_rr_list_rr_count(addr); i++) {
-		if (i > 0) {
-			fprintf(stdout, "\n");
-		}
-
 		if (ldns_resolver_push_nameserver_rr(res,ldns_rr_list_rr(addr, i)) != LDNS_STATUS_OK) {
 			printf("Error adding nameserver to resolver\n");
 		}
 
-		p = ldns_resolver_query(res, version, LDNS_RR_TYPE_TXT,LDNS_RR_CLASS_CH, LDNS_RD);
+		p = ldns_resolver_query(res, ldns_rr_list_rr(addr,i)->_rdata_fields[0], LDNS_RR_TYPE_TXT,LDNS_RR_CLASS_CH, LDNS_RD);
 		if (p) {
+			ldns_pkt_print(stdout,p);
 			printf("*********QUERY TIME:%u\n",ldns_pkt_querytime(p));
 			ldns_pkt_free(p);
 		} else {
@@ -83,7 +91,6 @@ int main(int argc, char *argv[]) {
 		}
 		ldns_rdf_deep_free(ldns_resolver_pop_nameserver(res));
 	}
-
 	ldns_rdf_deep_free(name);
 	ldns_resolver_deep_free(res);
 	exit(EXIT_SUCCESS);
