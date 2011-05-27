879eaceb4f0f4a426742a673caa3dc35aa7b0816
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-28 00:19:33 +0100

    added to_string method

--- Data.hxx
+++ Data.hxx
@@ -2,11 +2,20 @@
 #define DATA_HXX
 
 #include <mysql++.h>
+#include <xlocale.h>
 #include <iostream>
 #include <string>
 #include "Record.hxx"
 
 using namespace std;
+
+template <class T>
+std::string to_string(T t, std::ios_base & (*f)(std::ios_base&))
+{
+  std::ostringstream oss;
+  oss << f << t;
+  return oss.str();
+}
 
 class Data {
 	private:
