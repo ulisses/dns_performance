db0630aaeb5e42fe4298af615dd51ca02b98ba3a
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-28 00:18:41 +0100

    completed the fill method, now we are able to insert new stuff or update old one

--- Data.cxx
+++ Data.cxx
@@ -5,35 +5,63 @@ Data::Data(string server, string db, string user, string passwd) {
 	_dbname = db.c_str();
 	_username = user.c_str();
 	_password = passwd.c_str();
-	
-	/* Connect to the the database. */
-    mysqlpp::Connection _conn(false);
 
-    if (!_conn.connect(_dbname, _server, _username, _password)) {
-        cerr << "DB connection failed: " << _conn.error() << endl;
+	/* Connect to the the database. */
+    try {
+	    _conn = mysqlpp::Connection(_dbname,_server, _username, _password);
+    } catch (const mysqlpp::Exception& er) {
+		cerr << "Error making connection: " << er.what() << endl;
     }
 }
 
 void Data::fill(vector<Record*> *r) {
-	for(vector<Record*>::const_iterator it=r->begin(); it != r->end(); it++) {
-		Record *elem = *it;
+	try {
+		for(vector<Record*>::const_iterator it=r->begin(); it != r->end(); it++) {
+			Record *elem = *it;
+			mysqlpp::Query query = _conn.query("select * from dnsperf where webserver = %0q");
+			query.parse();
 
+			mysqlpp::SQLQueryParms sqp;
+			sqp << elem->getWebserver();
+			mysqlpp::StoreQueryResult res = query.store(sqp);
+			
+			if (res.empty()) {
+				/* We don't have this row in database, insert this new one*/
+				mysqlpp::Query newquery = _conn.query("insert INTO dnsperf (webserver,avgqtime,nrq,timefst,timelst) VALUES(%0q , %1 , 1 , %2q , %3q)");
+				newquery.parse();
+				
+				mysqlpp::SQLQueryParms sqpNew;
+				sqpNew << mysqlpp::String(elem->getWebserver())
+				       << mysqlpp::String(to_string<int>(elem->getTime(),std::dec))
+				       << mysqlpp::String(to_string<long>(elem->getTimestamp(),std::dec))
+				       << mysqlpp::String(to_string<long>(elem->getTimestamp(),std::dec));
+				newquery.store(sqpNew);
+			} else {
+				/* Update a row */
+				for (size_t i = 0; i < res.num_rows(); ++i) {
+					mysqlpp::Query newquery = _conn.query("update dnsperf SET webserver=%0q,avgqtime=%1,nrq=%2,timefst=%3q,timelst=%4q where webserver=%0q");
+					newquery.parse();
+
+					int newTime = (int) (atoi(res[i]["avgqtime"])+elem->getTime())/2;
+					int newNrq = (int) (atoi(res[i]["nrq"])+1);
+
+					mysqlpp::SQLQueryParms sqpNew;
+					sqpNew << mysqlpp::String(elem->getWebserver())
+					       << mysqlpp::String(to_string<int>(newTime,std::dec))
+					       << mysqlpp::String(to_string<int>(newNrq,std::dec))
+					       << mysqlpp::String(res[i]["timefst"])
+					       << mysqlpp::String(to_string<long>(elem->getTimestamp(),std::dec));
+					newquery.store(sqpNew);
+				}
+			}
+		}
+	} catch (const mysqlpp::BadQuery& er) {
+		/* Handles any query errors */
+		cerr << "Query error(fill): " << er.what() << endl;
+		exit(1);
+	} catch (const mysqlpp::Exception& er) {
+		// Catch-all for any other MySQL++ exceptions
+		cerr << "Error(fill): " << er.what() << endl;
+		exit(1);
 	}
 }
-
-        /*
-        // Retrieve a subset of the sample stock table set up by resetdb
-        // and display it.
-        mysqlpp::Query query = getResultFromQuery _conn.query("select item from stock");
-        if (mysqlpp::StoreQueryResult res = query.store()) {
-            cout << "We have:" << endl;
-            mysqlpp::StoreQueryResult::const_iterator it; 
-            for (it = res.begin(); it != res.end(); ++it) {
-                mysqlpp::Row row = *it;
-                cout << '\t' << row[0] << endl;
-            }
-        }
-        else {
-            cerr << "Failed to get item list: " << query.error() << endl;
-        }
-        */
