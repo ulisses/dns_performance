8cc68289267f5f4385a46767f91f2ceb50fe47e1
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-05-28 00:20:24 +0100

    add sleep and SIGINT mechanisms

--- Main.hxx
+++ Main.hxx
@@ -11,14 +11,36 @@
 
 using namespace std;
 
-Record *getRecordFromWebServer(string webserver) {
-	RecordC *r = getResultsForServer(webserver.c_str());
-	
-	string ns = string(r->nameserver);
-
-	return new Record(webserver, ns, r->time, r->timestamp);
+float getElapsedTimeInS() {
+	return clock()/CLOCKS_PER_SEC;
 }
 
+void sleep(int ms) {
+	float goal;
+	goal = ms + getElapsedTimeInS();
+
+	while( goal >= getElapsedTimeInS())
+		;
+}
+
+/* Convert a C typedef to a C++ class, we could use this typedef
+ * in C++, but is easier if we use object in the object's world.
+ */
+Record *getRecordFromWebServer(string webserver) {
+	RecordC *r = getResultsForServer(webserver.c_str());
+	if(!r) {
+		fprintf(stderr, "ERROR: could not get a RecordC from %s\n", webserver.c_str());
+		exit(-1);
+	} else {
+		string ns = string(r->nameserver);
+		return new Record(webserver, ns, r->time, r->timestamp);
+	}
+}
+
+/* We can improve this, by reading this list from a file
+ * or maybe implement in C++ the extraction of this information
+ * from the alexa.com website.
+ */
 vector<string> *getTopDomains() {
 	vector<string> *v = new vector<string>();
 	v->push_back("google.com");
